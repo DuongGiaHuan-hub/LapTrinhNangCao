10e2e9fa2266a4f100ae9ac0df64b236327fb697
DuongGiaHuan-hub <124770971+DuongGiaHuan-hub@users.noreply.github.com>
2023-02-08 16:18:44 +0700

    Add files via upload

---
+++ DoanBua.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if(x > 50) cout << "Chon the dang cam di";
+    else if(x <= 50) cout << "Chon the con lai";
+    return 0;
+}
