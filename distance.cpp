d4086f69d4f8b3b03dc9378f76ef3503a1779ade
DuongGiaHuan-hub <124770971+DuongGiaHuan-hub@users.noreply.github.com>
2023-02-08 16:06:52 +0700

    Add files via upload

---
+++ distance.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    double c;
+    c = sqrt(x*x +y*y);
+    cout << c;
+}
