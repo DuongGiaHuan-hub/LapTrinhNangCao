319286cb60a6af7fc6d1baccb88706f1a65e2213
DuongGiaHuan-hub <124770971+DuongGiaHuan-hub@users.noreply.github.com>
2023-02-08 16:40:45 +0700

    Add files via upload

---
+++ LapYear.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+   int a, b, c;
+   cin >> a >> b >> c;
+   if(a == b && b == c) cout << "true";
+   else cout << "false";
+    return 0;
+}
